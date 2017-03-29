b4d850acf2e1f930f86483dba9fb5ffe0d23aedc
Michael Walker <mike@barrucadu.co.uk>
2017-03-29 23:24:03 +0100

    Line type and renderer.

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Monad (foldM_)
 import Data.List (inits, nub, tails)
 import qualified Graphics.GD as GD
 
@@ -9,6 +10,35 @@ main = do
   putStrLn ("Got: '" ++ stdin ++ "'.")
   sizes <- getWordSizes stdin
   mapM_ (\(w,s) -> putStrLn ("'" ++ w ++ "': " ++ show s)) sizes
+  let width = 500
+  iota <- getStringSize " "
+  let ls = justify1 width sizes iota (words stdin)
+  render "out.png" width ls
+
+-- | A line of text is a non-empty list of words interspersed with
+-- spaces of varying sizes.
+data Line = Line String [(Int, String)]
+
+-- | Render a paragraph of text to an image.
+render :: String -> Int -> [Line] -> IO ()
+render fname width ls0 = do
+    ((_,y1), _, (_,y2), _) <- GD.measureString fontName fontSize 0 (0, 0) "l" 0
+    let lineheight = round (1.5 * fromIntegral (abs $ y2 - y1))
+    img <- GD.newImage (width, (length ls0 + 1) * lineheight)
+    GD.fillImage (GD.rgb 255 255 255) img
+    go img lineheight 1 ls0
+    GD.savePngFile fname img
+  where
+    go img lineheight = goLines where
+      goLines n (l:ls) = do
+        goLine n l
+        goLines (n+1) ls
+      goLines _ [] = pure ()
+
+      goLine n (Line w rest) = do
+        let y = n * lineheight
+        let r x s = (\(_, _, (x',_), _) -> x') <$> GD.drawString fontName fontSize 0 (x, y) s 0 img
+        foldM_ (\x (xoff, s) -> r (xoff+x) s) 0 ((0,w):rest)
 
 -- | Get the size of every word in a string.
 --
@@ -34,6 +64,17 @@ fragments s0 = s0 : concatMap go (zip (inits s0) (tails s0)) where
   go ([], _) = []
   go (_, []) = []
   go (h, t) = [h ++ "-", '-' : t]
+
+-------------------------------------------------------------------------------
+-- Text Justification
+
+-- | Justify a paragraph of text: first attempt, no wrapping at all.
+justify1 :: Int -> [(String, Int)] -> Int -> [String] -> [Line]
+justify1 _ _ iota (w:ws) = [Line w [(iota, w') | w' <- ws]]
+justify1 _ _ _ [] = []
+
+-------------------------------------------------------------------------------
+-- Font
 
 fontName :: String
 fontName = "/home/barrucadu/projects/justify/font.ttf"
